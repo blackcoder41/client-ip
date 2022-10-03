13b05765e1bd4282d8b09e6ce8f2da7e454bcabe
blackcoder41 <blackcoder41@gmail.com>
2022-10-03 21:45:17 +0800

    Retrieve HTTP headers

--- src/main/java/com/raketlabs/ip/client/controller/MainController.java
+++ src/main/java/com/raketlabs/ip/client/controller/MainController.java
@@ -2,17 +2,20 @@ package com.raketlabs.ip.client.controller;
 
 import com.raketlabs.ip.client.response.GetClientIpResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @RestController
 public class MainController {
 
     @GetMapping("getClientIp")
-    public GetClientIpResponse getClientIp (HttpServletRequest httpServletRequest) {
+    public GetClientIpResponse getClientIp (HttpServletRequest httpServletRequest, @RequestHeader Map<String, String> headers) {
         GetClientIpResponse response = new GetClientIpResponse();
         response.setIp(httpServletRequest.getRemoteAddr());
+        response.setHeaders(headers);
         return response;
     }
 }
--- src/main/java/com/raketlabs/ip/client/response/GetClientIpResponse.java
+++ src/main/java/com/raketlabs/ip/client/response/GetClientIpResponse.java
@@ -2,7 +2,10 @@ package com.raketlabs.ip.client.response;
 
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 public class GetClientIpResponse {
     String ip;
+    Map<String, String> headers;
 }
