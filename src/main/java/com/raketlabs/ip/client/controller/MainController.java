979d64d0a146af11189b02a9aea26bfd670b483c
blackcoder41 <blackcoder41@gmail.com>
2022-10-04 13:50:51 +0800

    Log the response

--- src/main/java/com/raketlabs/ip/client/controller/MainController.java
+++ src/main/java/com/raketlabs/ip/client/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.raketlabs.ip.client.controller;
 
 import com.raketlabs.ip.client.response.GetClientIpResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,14 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
+@Slf4j
 @RestController
 public class MainController {
 
     @GetMapping("getClientIp")
     public GetClientIpResponse getClientIp (HttpServletRequest httpServletRequest, @RequestHeader Map<String, String> headers) {
-        GetClientIpResponse response = new GetClientIpResponse();
+        var response = new GetClientIpResponse();
         response.setIp(httpServletRequest.getRemoteAddr());
         response.setHeaders(headers);
+        log.info("response: {}", response);
         return response;
     }
 }
